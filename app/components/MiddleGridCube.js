a1fad4306ee0512800f3a538d643e380ff267555
Dillan <dillyferster@gmail.com>
2024-03-28 16:10:27 -0600

    update

--- app/components/MiddleGridCube.js
+++ app/components/MiddleGridCube.js
@@ -4,7 +4,7 @@ import Globe from "../assets/globe.svg";
 export function MiddleGridCube() {
   return (
     <div className="border-2 border-black w-[21.42rem] h-[19rem] rounded-sm  bg-transparent flex flex-col justify-evenly items-center transition-all overflow-hidden  ">
-      <div className="text-sm">YOU CAN TAKE ONE MORE DAY OFF IT'S FINE</div>
+      <div className="text-sm">YOU CAN TAKE ONE MORE DAY OFF ITS FINE</div>
       <div>
         <Image src={Globe} alt="globe" />
       </div>
