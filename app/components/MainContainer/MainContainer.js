8b761a998406edd7c8559d36bc155fb658835a79
Dillan <dillyferster@gmail.com>
2024-04-17 16:02:30 -0600

    responsive update

--- app/components/MainContainer/MainContainer.js
+++ app/components/MainContainer/MainContainer.js
@@ -19,10 +19,10 @@ export function MainContainer({ flightData }) {
   return (
     <div
       id="MainC"
-      className="flex justify-center px-4 mt-4 max-w-[62rem] max-lg:w-[50rem]  mx-auto max-md:w-[38rem] max-md:mt-2 max-sm:w-[25rem]"
+      className="flex justify-center px-4 mt-4 max-w-[62rem] max-lg:w-[46rem]  mx-auto max-md:w-[38rem] max-md:mt-2 max-sm:w-[20rem]"
     >
       <Link href={`/details/0`}>
-        <div className="flex bg-[url('/Beach.jpeg')] bg-cover  bg-left justify-end items-end border-2 border-[#696565] border-solid  max-w-[62rem] rounded-2xl h-48 hover:translate-y-[1px] drop-shadow-[_1px_6px_#757272] ease-in-out transition-all hover:drop-shadow-[_1px_5px_#9A9797] overflow-hidden cursor-pointer [&>*]:hover:backdrop-blur-sm max-lg:w-[44rem] max-md:w-[36rem] max-sm:w-[30rem] ">
+        <div className="flex bg-[url('/Beach.jpeg')] bg-cover  bg-left justify-end items-end border-2 border-[#696565] border-solid  max-w-[62rem] rounded-2xl h-48 hover:translate-y-[1px] drop-shadow-[_1px_6px_#757272] ease-in-out transition-all hover:drop-shadow-[_1px_5px_#9A9797] overflow-hidden cursor-pointer [&>*]:hover:backdrop-blur-sm max-lg:w-[44rem] max-md:w-[36rem] max-sm:w-[23rem] ">
           <MainContainerButton flightData={flightData}></MainContainerButton>
         </div>
       </Link>
--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="bg-fixed bg-[var(--color-bg)] scroll-smooth">
+    <html lang="en" className=" bg-[var(--color-bg)] scroll-smooth">
       <body className={inter.className}>
         <Nav />
 
