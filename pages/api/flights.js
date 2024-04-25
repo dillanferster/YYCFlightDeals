a764f23762802e938c215b90470335eac72c4cff
Dillan <dillyferster@gmail.com>
2024-04-24 18:11:47 -0600

    Revert "env update"
    This reverts commit 62332e6a331f9f12702e3752d2714a62bef7814e.

--- pages/api/flights.js
+++ pages/api/flights.js
@@ -1,18 +1,13 @@
 // pages/api/flights.js
 export default async function handler(req, res) {
   // Define the URL of the external API you want to fetch data from
+  const url =
+    "https://api.travelpayouts.com/v1/city-directions?origin=YYC&limit=14&currency=cad&market=ca&token=f3e7c8d52cbadd71315114a9744c4d01";
+  // "https://api.travelpayouts.com/v1/prices/cheap?origin=YYC&destination=&currency=cad&market=ca&token=f3e7c8d52cbadd71315114a9744c4d01";
 
   try {
     // Fetch data from the external API
-    const response = await fetch(
-      "https://api.travelpayouts.com/v1/city-directions?origin=YYC&limit=14&currency=cad&market=ca",
-      {
-        method: "GET",
-        headers: {
-          "x-access-token": process.env.NEXT_PUBLIC_FLIGHT_API_KEY,
-        },
-      }
-    );
+    const response = await fetch(url);
     const data = await response.json();
 
     // Set the CORS headers
